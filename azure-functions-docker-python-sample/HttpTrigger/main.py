bb26fb43be97f2491d8923dc4781b62acfe58bbf
Singh Shekhawat <ajit.singh-shekhawat@capgemini.com>
2023-06-27 08:21:05 +0200

    pyping

--- azure-functions-docker-python-sample/HttpTrigger/main.py
+++ azure-functions-docker-python-sample/HttpTrigger/main.py
@@ -1,5 +1,5 @@
 import logging
-
+import pyping
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -15,7 +15,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello {name}!")
+        r = pyping.ping('google.com')
+        if r.ret_code == 0:
+            return func.HttpResponse(f"Success with {r.ret_code}. IP address if {r.destination_ip}")
+        else:
+            return func.HttpResponse(f"Failed with {r.ret_code}.")
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
